2270a214b6766913d386aee3796f80a4b1224382
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-01-14 09:43:23 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="GPSTiming",
     description="Interpolate FACT boardtime stamps with GPS triggers",
-    version="0.1",
+    version="0.2",
     author="Noah Biederbeck",
     author_email="noah.biederbeck@tu-dortmund.de",
     packages=find_packages(),
